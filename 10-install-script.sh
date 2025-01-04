20081fdc557d3982cc5b51d96689a3513162f18e
thatikayalasandeep <sandeepthatikayala4@gamil.com>
2025-01-04 12:31:57 +0530

    Intial message for installing script

---
+++ 10-install-script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if (USERID -ne 0)
+then 
+    echo "you must have root access to install the packages"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if ($? -ne 0)
+then
+    dnf install mysql -y
+    if($? -ne 0)
+    then
+        echo "Installing Mysql................Failed"
+        exit 1
+    else
+        echo "Installing Mysql...............Success"
+    fi
+
+else
+    echo "Mysql is already...........Installed"
+
+fi
+
+dnf list installed git
+
+if ($? -ne 0)
+then
+    dnf install git -y
+    if ($? -ne 0)
+    then
+        echo "Installing git ................Failed"
+    else
+        echo "Installing git ................Success"
+    fi
+    
+else
+    echo "Git is already ...........Installed"
+fi
