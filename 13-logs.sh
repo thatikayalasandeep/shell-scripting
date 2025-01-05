bdcf84b7879841828ea04065e89b8d545eab86d3
thatikayalasandeep <sandeepthatikayala4@gamil.com>
2025-01-05 08:02:04 +0530

    removed unwanted line

--- 13-logs.sh
+++ 13-logs.sh
@@ -7,7 +7,6 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-"/home/ec2-user/shell-scripting"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
